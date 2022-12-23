12705f25370f9db38938077e69a57b863db9b454
ArturoLunaa <arturolunaago@hotmail.com>
2022-12-23 16:51:33 -0600

    Proyecto terminado y funcionando

---
+++ AppTriLocCam/Triggers/AgeTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppTriLocCam.Triggers
+{
+    public class AgeTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            int n;
+            bool isNumeric = int.TryParse(sender.Text, out n);
+            if (!isNumeric)
+            {
+                //No es un número
+                sender.Text = "";
+            }
+            else
+            {
+                //Es numérico
+                if(n < 0)
+                {
+                    //Si es menor que cero regresamos cero
+                    sender.Text = "0";
+                }
+                else if(n > 99)
+                {
+                    //Si es mayor que 99 regresamos 99
+                    sender.Text = "99";
+                }
+            }
+                
+        }
+    }
+}
---
+++ AppTriLocCam/ViewModels/CameraViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppTriLocCam.ViewModels
+{
+    public class CameraViewModel : BaseViewModel
+    {
+        //Comandos
+        private Command _TakePictureCommand;
+        public Command TakePictureCommand => _TakePictureCommand ?? (_TakePictureCommand = new Command(TakePictureAction));
+
+        //Propiedades
+        private string _Picture;
+
+        public string Picture 
+        { 
+            get => _Picture;
+            set => SetProperty(ref _Picture, value);       
+        }
+
+        //Constructor
+        public CameraViewModel()
+        {
+
+        }
+        //Métodos
+        private async void TakePictureAction()
+        {
+            //Inicializa la cámara
+                await CrossMedia.Current.Initialize();
+
+            //Si la camara no está disponible o no está soportada lanza un mensaje y termina este método
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await Application.Current.MainPage.DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                //Toma la fotografía
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "AppTriLocCam",
+                    Name = "cam_picture.jpg"
+                });
+
+                //Si el objeto file es null termina este método
+                if (file == null)
+                    return;
+
+                //Asignamos la ruta de la fotografóa en la propiedad de la imagen
+                Picture = file.Path;
+               
+                /*image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });*/
+
+        }
+    }
+}
